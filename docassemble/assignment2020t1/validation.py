908514caca2667839720a8676382fed6e389d63a
Anonymous User <70843711+nikkisunnn@users.noreply.github.com>
2020-10-09 22:16:43 +0800

    (1) Added checksum algorithm in validation py module, (2) Added more things in welcome msg, (3) Defined what savings and investments are for the applicant

--- docassemble/assignment2020t1/validation.py
+++ docassemble/assignment2020t1/validation.py
@@ -1,7 +1,26 @@
 import re 
 from docassemble.base.util import *
 
-def check_nric(string):
-  if not re.match(r'[STst][0-9]{7}[A-z]', string) or len(string) > 9:
-    validation_error('Invalid NRIC format.')
-  return True
+def check_nric(nric):
+    nricCheckDigits = 'JZIHGFEDCBA'
+    weights = [2, 7, 6, 5, 4, 3, 2]
+    if len(nric) != 9:
+        return False
+
+    firstChar = nric[0].upper()
+    digits = nric[1:8]
+    lastChar = nric[8].upper()
+    
+    if firstChar not in "ST" or not digits.isdigit():
+        return False
+
+    total = 0
+    if firstChar in "T":
+        total += 4
+    for i in range(7):
+        total += int(digits[i]) * weights[i]
+
+    if firstChar in "ST"and nricCheckDigits[total % 11] == lastChar:
+      return True
+    else:
+      return False
